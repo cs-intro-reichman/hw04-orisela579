e059eb416eac622152572df02a325b1b8f66167f
Ori Sela <orisela579@gmail.com>
2025-11-23 11:05:59 +0200

    myString third:)

--- MyString.java
+++ MyString.java
@@ -32,8 +32,11 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        if(str1.length()==0 || str2.length()==0){
+        if(str1.length()==0){
             return false;
+        }
+        if (str2.length()==0) {
+            return true;
         }
         if(str1.length()<str2.length()){
             return false;
